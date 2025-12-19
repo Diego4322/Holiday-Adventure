8c7d0ed0687bb94a7f7a31ff3766b70fcbe26072
Diego4322 <gldenpvp1@gmail.com>
2025-12-19 13:25:28 +0000

    unfinished

---
+++ Game.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+
+public class Game{
+    public static void main(String[] args){
+         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter hero name: ");
+
+        String name = input.nextLine();
+
+        System.out.println(name + " was taking a stroll across the village. \nThen " + name + " heard a scream from a house. Someone was calling for help!" + "\nAs " + name +  "sprinted across to save the person in need, they suddenly he saw a evil Santa. What should he do?");
+
+        System.out.println("Please type either A or B");
+        System.out.println("Option A: Save the person ");
+        System.out.println("Option B: Run away");
+        System.out.println("What is your choice? ");
+        
+
+
+        String Choice1 = input.nextLine();
+
+        if (Choice1 = "A"){
+
+        }
+
+
+    }
+}
